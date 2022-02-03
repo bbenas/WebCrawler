664b3a21293e9854057df991459c4563f89fa5fb
Benas Bulota <b.bulota1@newcastle.ac.uk>
2022-02-03 16:29:19 +0000

    Added a test suite

---
+++ src/Tests/CSVTest.java
@@ -0,0 +1,37 @@
+package Tests;
+
+import main.CSVHandler;
+import main.WebCrawler;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.PrintWriter;
+import java.util.*;
+
+public class CSVTest {
+
+    @Test
+    public void checkCreateCSVReturnObject() {
+        try {
+            PrintWriter actualFile = CSVHandler.createCSV("test.csv", new HashSet<String>(Arrays.asList("a", "b", "c")));
+            Assert.assertEquals(PrintWriter.class, actualFile.getClass());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testIfGetTotalReturnsCorrect() {
+        int actualResult = WebCrawler.getTotal("Text,10,20,30,40");
+        int expectedValue = 100;
+        Assert.assertEquals(expectedValue, actualResult);
+    }
+
+    @Test
+    public void testIfGetTotalIgnoresHeader() {
+        int actualResult = WebCrawler.getTotal("URL,Data1,Data2,Data3");
+        int expectedValue = -1;
+        Assert.assertEquals(expectedValue, actualResult);
+    }
+}
